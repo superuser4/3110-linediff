7210d4e3b6875d05b491449e737176c0a75fef05
superuser4 <superuser3344@protonmail.com>
2025-11-24 22:33:51 -0500

    understood assignment i think

--- src/simcheck.py
+++ src/simcheck.py
@@ -41,8 +41,9 @@ class SimilarityChecker:
                     del(file2_lines[j])
                     break
         
-        # deleting the other lines removed original context similarity? possible mess up?
         # if line doesn't match 1-1 we check this line against all possible new lines within appropiate distance using LHDiff algorithm
+        # deleting the other lines removed original context similarity? possible mess up?
+        # run everything through LHDiff check instead of manual check?
 
 
         return hash_map
