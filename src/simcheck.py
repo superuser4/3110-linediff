4bd88b3efcdc51219ebacc41b96bb046de095fd4
superuser4 <superuser3344@protonmail.com>
2025-12-07 16:52:03 -0500

    corrected the line comparison func

--- src/simcheck.py
+++ src/simcheck.py
@@ -1,4 +1,5 @@
 from os import _exit 
+import diff 
 
 class SimilarityChecker:
     file1 = ""
@@ -21,27 +22,53 @@ class SimilarityChecker:
         return lin1, lin2
 
 
-    def line_comp(self):
+    def line_comp(self, similar_threshold=0.8):
         file1_lines, file2_lines = self.file_parser()
         hash_map = {}
         used_file2 = set()
         
-        for i in range(len(file1_lines)):
-            left_context_vec = []
-            right_context_vec = []
-
-            for j in range(len(file2_lines)):
+        for i, line1 in enumerate(file1_lines):
+            if line1 == "":
+                continue
+            for j, line2 in enumerate(file2_lines):
                 if j in used_file2:
-                    break
-                if file1_lines[i] == file2_lines[j]:
-                    if file1_lines[i] == "":
-                        continue
-                    
+                    continue
+                if line1==line2:
                     hash_map[i+1] = j+1
+                    used_file2.add(j)
                     break
-        
 
+        ### For approximate matches
+        for i, line1 in enumerate(file1_lines):
+            ## alr matched or empty line
+            if (i+1) in hash_map or line1== "":
+                continue
+            
+            best_j = None
+            best_scor = 0
+
+            for j, line2 in enumerate(file2_lines):
+                left_context_file1 = file1_lines[max(0, i-1):i]
+                right_context_file1 = file1_lines[i+1:min(len(file1_lines), i+2)]
+
+                score_o = diff.SimilarityScore(
+                        line1=line1,
+                        line2=line2,
+                        left_context_vec=left_context_file1,
+                        right_context_vec=right_context_file1
+                )
+                sim_score = score_o.lhdiff_check()
+                if sim_score > best_scor:
+                    best_scor = sim_score
+                    best_j = j
+
+            if best_j is not None and best_scor >= similar_threshold:
+                hash_map[i+1] = best_j+1
+                used_file2.add(best_j)
         return hash_map
+
+    
+         
     def check(self):
         hash_map = self.line_comp()
         print("Line matches:")
