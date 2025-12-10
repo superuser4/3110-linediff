01114df2717f7db6f0da33f96389d8611c9553cb
Mona <mona4moussa@gmail.com>
2025-12-10 17:07:26 -0500

    add another program

---
+++ src/newDataset/shoppingCart_v1.py
@@ -0,0 +1,21 @@
+#basic shopping cart
+cart = []
+
+def add_item(item):
+    cart.append(item)
+    print("Added:", item)
+
+def total_items():
+   return len(cart)
+
+add_item("Apples")
+add_item("Bananas")
+add_item("Grapes")
+
+print("Cart contains:", cart)
+print("Count", total_items())
+
+print("Done shopping.")
+
+
+
---
+++ src/newDataset/shoppingCart_v2.py
@@ -0,0 +1,22 @@
+cart = []
+
+def add_item(item):
+    if item not in cart:         
+        cart.append(item)
+        print("Added:", item)
+
+def remove_item(item):            
+    if item in cart:
+         cart.remove(item)
+         print("Removed:", item)
+
+def total_items():
+   return len(cart)
+
+add_item("Apples")
+add_item("Bananas")
+add_item("Apples")
+remove_item("Apples")
+
+print("Current cart:", cart)      
+print("Count:", total_items())
